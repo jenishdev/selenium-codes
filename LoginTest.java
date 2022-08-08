588ca330de074419c2e5b434fac36aeb47cdf624
JENISH DEV <46190894+jenishdev@users.noreply.github.com>
2022-08-08 18:20:31 +0530

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,20 @@
+package mavendemo.functinaltesting;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void login() 
+	{
+		System.out.println("maven appium");	
+		}
+	@Test
+	public void transfermoney() 
+	{
+		
+		System.out.println("transfermoney maven");
+	}
+
+
+}
---
+++ UiTest.java
@@ -0,0 +1,19 @@
+package mavendemo.functinaltesting;
+
+import org.testng.annotations.Test;
+
+public class UiTest {
+	@Test
+	public void draganddrop() 
+	{
+		
+		System.out.println("draganddrop maven");
+	}
+    @Test
+	public void tap() 
+    {
+		
+		System.out.println("tap maven");
+	}
+
+}
